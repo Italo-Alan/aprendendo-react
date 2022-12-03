6b0ee47b71be966ebf59177c56710e4f56dc9861
Italo-Alan <italoalan123@gmail.com>
2022-12-02 22:06:03 -0300

    Subindo primeiro projeto React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/login" element ={<Login/>}/>
+      </Routes>
+    </Router>
+
+
+
+
+    // <div className="App">
+    //   <h1>
+    //     Olá, mundo!
+    //   </h1>
+    //   <Button title="Entrar"/>
+    //   <Button title="Sair"/>
+    //   <Button title="Fechar"/>
+    //   <Button title="Salvar"/>
+    // </div>
   );
 }
 
